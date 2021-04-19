f3bef9b2362848fa2581feabddf8b5321fb3cf07
Anthony <ayoskovich@nhaschools.com>
2021-04-19 17:04:21 -0400

    Updating gitignore

--- src/Notebook Overview.py
+++ src/Notebook Overview.py
@@ -3,6 +3,36 @@
 
 # # Jupyter Notebook Overview
 
+# This is a jupyter notebook. Within it, there are cells that either contain markdown syntax or Python code. This cell contains markdown, while the one underneath it contains code. To execute the Python code below you can either:
+# - Click the cell, and click the "Run" button in the above menu
+# - Click the cell, and push control+enter
+
+# In[ ]:
+
+
+print('Hello!')
+
+
+# Variables that you assign in one cell are available to all other cells.
+
+# In[ ]:
+
+
+x = 10
+
+
+# In[ ]:
+
+
+x
+
+
+# **Warning!** When using a Jupyter notebook, simply running a cell with a variable in it will display the value below. However, this behavior is not always the case when using other environments besides a Jupyter Notebook.
+
+# --- 
+
+# You can also start a cell with `!` and use a code cell as a terminal (cmd or powershell, this notebook should be using cmd)
+
 # In[ ]:
 
 
